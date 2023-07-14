2277624999c01db7f1eba0b860fca98583875c2f
mohammadsaleh763 <74305278+mohammadsaleh763@users.noreply.github.com>
2023-07-14 11:14:13 +0500

    Add files via upload

---
+++ task22.js
@@ -0,0 +1,5 @@
+var mountains = ["Mount Everest", "Nanga Parbat", "Himalaya", "Karakoram"]; //array
+var invalidIndex = 8; //declare invalid index
+console.log(mountains[invalidIndex]); //error occurs at this index no element found
+var validIndex = 2;
+console.log(mountains[validIndex]);
---
+++ task26.ts
@@ -0,0 +1,17 @@
+let alien_color = 'green';
+
+// Version that runs the if block
+if (alien_color === 'green') {
+  console.log("Congratulations! You just earned 5 points for shooting the alien.");
+} else {
+  console.log("Congratulations! You just earned 10 points.");
+}
+
+// Version that runs the else block
+alien_color = 'red';
+
+if (alien_color === 'green') {
+  console.log("Congratulations! You just earned 5 points for shooting the alien.");
+} else {
+  console.log("Congratulations! You just earned 10 points.");
+}
